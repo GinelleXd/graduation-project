f2d24e41e399882e8015711286043988308762ec
ginelle <47771607+GinelleXd@users.noreply.github.com>
2019-06-04 17:04:33 +0800

    Update index.js

--- backstage/js/index.js
+++ backstage/js/index.js
@@ -59,6 +59,9 @@ window.onload = function () {
     if (str == "") {
         $("#js_userName").val("未登录");
         $("#js_logout").addClass("none");
+        $("#js_userName").on("click",function(){
+            location.href("https://ginellexd.github.io/graduation-project/backstage/login.html");
+        })
     } else {
         $("#js_userName").html(aname);
         $("#js_logout").removeClass("none");
